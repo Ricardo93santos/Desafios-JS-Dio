151f2b534778763219b13b8406762b6fd6dc8792
Ricardo Santos <ricardo93ssantos@gmail.com>
2022-05-16 15:38:31 -0300

    ok

--- To-Do-List/js/script.js
+++ To-Do-List/js/script.js
@@ -3,18 +3,13 @@ window.addEventListener("load", addEventos)
 function addEventos(){
 
   const btn_submit = document.querySelector("#btn_submit")
-
+  const arrayItem = []
   btn_submit.addEventListener("click", adicionarItem)
+
 
   function adicionarItem(){
     let text_input = document.querySelector("#text_input").value
     let list_item = document.querySelector("#list_item")
-    let arrayItem = []
-
-    for (let i = 0; i < arrayItem.length; i++) {
-      
-
-    }
-
+    list_item.innerHTML= text_input  
   }
 }
